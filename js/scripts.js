093dce6a071e8cc655fd05b8856e53383f4abc1a
Julio Alcantara <jalcantara88@gmail.com>
2020-11-28 13:21:33 -0800

    changed modal to open with JQuery

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,21 @@ $(function() {
         if ($("#carouselButton").children("i").hasClass("fa-pause")){
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play")
+            $("#carouselButton").children("i").addClass("fa-play");
         }
-        else{
+        else{                          
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause");
         }
+    });
+
+    $("#reserveButton").click(function() {
+        $("#modalReserveCampsite").modal("show");
+    });
+
+    $("#loginButton").click(function() {
+        $("#loginModal").modal("show");
     });
 
 });
